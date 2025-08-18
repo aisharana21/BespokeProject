9d3e582b5adecfd60bbf838a18332c2eca1eb098
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-18 11:50:33 +0500

    Switch to HashRouter for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+// <BrowserRouter  basename="/Bespoke-Project" >
+//     <App />
+// </BrowserRouter >
+//   </StrictMode>,
+// )
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<BrowserRouter  basename="/Bespoke-Project" >
-    <App />
-</BrowserRouter >
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
