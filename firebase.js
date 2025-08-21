7322e623a4fb82e744ebc2f4296b3f12e61cd316
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-21 15:34:19 +0500

    set up firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyCFJpbmrAzPHHUNEC06aT59c_E36_q13P0",
+  authDomain: "bespoke-693a6.firebaseapp.com",
+  projectId: "bespoke-693a6",
+  storageBucket: "bespoke-693a6.firebasestorage.app",
+  messagingSenderId: "882937773913",
+  appId: "1:882937773913:web:5b7788698815f55a241648",
+  measurementId: "G-0E2T4BVT0X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
