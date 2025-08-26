14161993fb68ef277d6e128947d5cfcfba379034
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-26 12:02:07 +0500

    faq completed

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,8 +1,37 @@
+import { faqData } from "./data/faqdata";
+import ArrowButton from '../assets/arrow-24.png';
+import { useState } from "react";
+import './CSS/faq.css';
 
-export  function FAQ() {
+export function FAQ() {
+  const [isActiveId, setIsActiveId] = useState(false);
+
+  const showFaqAnswer = (id) => {
+    setIsActiveId(isActiveId === id ? false : id);
+  };
+
   return (
-    <div>
-      cccccccccccc
+    <div className="faq-container">
+     <h2>Frequently Asked Questions</h2>
+      {faqData.map((data) => (
+        <div className={isActiveId === data.id ? "faq active" : "faq"} key={data.id}>
+          <div className="faq-question-answer">
+            <div className="tab-title">
+              <h5>  { data.question }
+</h5>
+              <button
+                onClick={() => showFaqAnswer(data.id)}
+                className="tab-open-arrow"
+              >
+                <img src={ArrowButton} alt="Toggle" />
+              </button>
+            </div>
+            <div className="faq-answer">
+              {data.answer}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
